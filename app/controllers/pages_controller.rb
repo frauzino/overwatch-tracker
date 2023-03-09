77536c9565693ec0d2107a98826c0374435f0189
Richard Young <richarddfyoung@gmail.com>
2023-03-09 09:21:40 -0500

    fixed problem with win rate calculation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
-  before_action :set_matches_played, only: %i[home match_win_rate]
+  before_action :set_matches_played, only: %i[home match_win_rates]
+  before_action :set_heroes_played, only: %i[home hero_win_rates]
+  before_action :set_maps_played, only: %i[home map_win_rates]
   before_action :match_win_rates, only: %i[home]
   before_action :hero_win_rates, only: %i[home]
   before_action :map_win_rates, only: %i[home]
@@ -10,13 +12,13 @@ class PagesController < ApplicationController
     @match_count = @matches.map { |match| [match.map, match.hero]}.tally
     @maps = Map.all
     @map_names = @maps.map(&:name)
-    @maps_played = (@matches.map(&:map)).uniq
-    @map_count = (@matches.map(&:map)).tally
+    @map_count = @matches.map(&:map).tally
     @heroes = Hero.all
     @hero_names = @heroes.map(&:name)
-    @heroes_played = (@matches.map(&:hero)).uniq
-    @hero_count = (@matches.map(&:hero)).tally
+    @hero_count = @matches.map(&:hero).tally
   end
+
+  private
 
   def match_win_rates
     @match_win_rates = []
@@ -31,8 +33,8 @@ class PagesController < ApplicationController
 
   def hero_win_rates
     @hero_win_rates = []
-    @matches_played.each do |match|
-      matches = Match.where(hero_id: match.hero)
+    @heroes_played.each do |hero|
+      matches = Match.where(hero_id: hero)
       count = matches.count
       wins = matches.where(win: true).count.to_f
       @hero_win_rates << "#{(wins / count * 100).to_i}%"
@@ -42,8 +44,8 @@ class PagesController < ApplicationController
 
   def map_win_rates
     @map_win_rates = []
-    @matches_played.each do |match|
-      matches = Match.where(map_id: match.map)
+    @maps_played.each do |map|
+      matches = Match.where(map_id: map)
       count = matches.count
       wins = matches.where(win: true).count.to_f
       @map_win_rates << "#{(wins / count * 100).to_i}%"
@@ -51,9 +53,15 @@ class PagesController < ApplicationController
     return @map_win_rates
   end
 
-  private
-
   def set_matches_played
     @matches_played = Match.all.uniq { |match| [match.map, match.hero] }
+  end
+
+  def set_heroes_played
+    @heroes_played = Match.all.map(&:hero).uniq
+  end
+
+  def set_maps_played
+    @maps_played = Match.all.map(&:map).uniq
   end
 end
